c21e85acf788e91c0640be66d38ebd03a6b7279a
781245153@qq.com <781245153@qq.com>
2020-09-09 21:30:07 +0800

    完成了登录页布局

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,27 +2,82 @@
     登录的路由组件
 */
 import React, { Component } from "react";
+import { Form, Input, Button } from "antd";
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
-import "./login.less"
-import logo from "./images/logo.png"
-
+import "./login.less";
+import logo from "./images/logo.png";
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
   }
+
+  onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
   render() {
     return (
-        <div className="login">
-            <header className="login-header">
-              <img src={logo} alt="logo"/>
-              <h1>React: 后台管理项目</h1>
-            </header>
-            <section className="login-content">
-              <h2>用户登陆</h2>
-              
-            </section>
-        </div>
+      <div className="login">
+        <header className="login-header">
+          <img src={logo} alt="logo" />
+          <h1>React: 后台管理项目</h1>
+        </header>
+        <section className="login-content">
+          <h2>用户登陆</h2>
+
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={this.onFinish}
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Username!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Password!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type="primary"
+                block
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
+        </section>
+      </div>
     );
   }
 }
